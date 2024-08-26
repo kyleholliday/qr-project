0321785a6b65e6e09f776522681131d13ec9d9fc
Kyle Holliday <kyleholliday@users.noreply.github.com>
2024-08-26 10:27:05 -0400

    Delete api directory

--- api/scan-and-redirect.js
+++
@@ -1,7 +0,0 @@
-const { incrementScanCount } = require('../server.js');
-
-export default function handler(req, res) {
-  const updatedScanCount = incrementScanCount(100);
-  console.log(`QR code scanned. Scan count is now ${updatedScanCount}`);
-  res.redirect('/');
-}
